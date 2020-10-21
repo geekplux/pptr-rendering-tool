fc7647e8612a40f1c2b5680e1b1a1fc9f173e424
geekplux <geekplux@gmail.com>
2020-10-21 20:50:41 +0800

    feat: using pptr core and chrome-aws-lambda

--- setup.js
+++ setup.js
@@ -1,4 +1,7 @@
+const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer');
+const puppeteerCore = require('puppeteer-core');
+const fonts = require('./fonts');
 
 const isBrowserAvailable = async (browser) => {
   try {
@@ -20,16 +23,31 @@ exports.getBrowser = (() => {
     // eslint-disable-next-line no-return-assign
     launching = new Promise((r) => (unlock = r));
     if (!browser || !(await isBrowserAvailable(browser))) {
-      browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '-—disable-dev-tools',
-        ],
-        dumpio: true,
-        devtools: false,
-      });
+      if (process.env.NODE_ENV === 'development' && puppeteer) {
+        browser = await puppeteer.launch({
+          args: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '-—disable-dev-tools',
+          ],
+          dumpio: true,
+          devtools: false,
+        });
+      }
+
+      if (process.env.NODE_ENV === 'production' && puppeteerCore) {
+        await Promise.all(
+          fonts.map(async (font) => {
+            await chrome.font(font);
+          })
+        );
+        browser = await puppeteerCore.launch({
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        });
+      }
 
       console.log(`launch done: ${await browser.version()}`);
     }
