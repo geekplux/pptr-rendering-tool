38981eebaa6511124a81e23d02305401bf8906fc
geekplux <geekplux@gmail.com>
2020-10-21 21:10:14 +0800

    fix: drop font code

--- setup.js
+++ setup.js
@@ -1,13 +1,13 @@
 const chrome = require('chrome-aws-lambda');
 let puppeteer
-// try {
-//   puppeteer = require('puppeteer');
-// } catch (error) {
-//   console.error(error)
-// }
+try {
+  puppeteer = require('puppeteer');
+} catch (error) {
+  console.error(error)
+}
 
 const puppeteerCore = require('puppeteer-core');
-const fonts = require('./fonts');
+// const fonts = require('./fonts');
 
 const isBrowserAvailable = async (browser) => {
   try {
@@ -29,25 +29,25 @@ exports.getBrowser = (() => {
     // eslint-disable-next-line no-return-assign
     launching = new Promise((r) => (unlock = r));
     if (!browser || !(await isBrowserAvailable(browser))) {
-      // if (process.env.NODE_ENV === 'development' && puppeteer) {
-      //   browser = await puppeteer.launch({
-      //     args: [
-      //       '--no-sandbox',
-      //       '--headless',
-      //       '--disable-gpu',
-      //       '-—disable-dev-tools',
-      //     ],
-      //     dumpio: true,
-      //     devtools: false,
-      //   });
-      // }
+      if (process.env.NODE_ENV === 'development' && puppeteer) {
+        browser = await puppeteer.launch({
+          args: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '-—disable-dev-tools',
+          ],
+          dumpio: true,
+          devtools: false,
+        });
+      }
 
       if (process.env.NODE_ENV === 'production' && puppeteerCore) {
-        await Promise.all(
-          fonts.map(async (font) => {
-            await chrome.font(font);
-          })
-        );
+        // await Promise.all(
+        //   fonts.map(async (font) => {
+        //     await chrome.font(font);
+        //   })
+        // );
         browser = await puppeteerCore.launch({
           args: chrome.args,
           executablePath: await chrome.executablePath,
